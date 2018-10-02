c7268ca53052af8c0f6002f8b91c10929db71919
Johannes Merkert <johannes.merkert@gmail.com>
2018-10-03 01:45:41 +0200

    Initial commit

---
+++ src/test/java/com/merkert/morris/AppTest.java
@@ -0,0 +1,14 @@
+package com.merkert.morris;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
